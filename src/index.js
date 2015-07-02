c15cc32553b3fddf1a2f7b0dd793b74755cb4fe4
Jeffrey Heer <jeffrey.heer@gmail.com>
2015-07-02 10:16:38 -0700

    Add stream summary to index.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
-  Bloom:        require('./bloom'),
-  CountMin:     require('./count-min'),
-  CountMeanMin: require('./count-mean-min'),
-  NGram:        require('./ngram')
+  Bloom:         require('./bloom'),
+  CountMin:      require('./count-min'),
+  CountMeanMin:  require('./count-mean-min'),
+  NGram:         require('./ngram'),
+  StreamSummary: require('./stream-summary')
 };
